a4b9e9f146cbd7404c81675f98205db3e882e467
Kozma Márton <kozmamarton76@gmail.com>
2026-05-04 14:48:25 +0200

    build mlir in debug mode

--- scripts/build_mlir.sh
+++ scripts/build_mlir.sh
@@ -29,7 +29,7 @@ cmake -G Ninja -S "$PROJECT_ROOT" -B "$BUILD_DIR" \
   -DLLVM_DIR=$PROJECT_ROOT/externals/llvm-project/build/lib/cmake/llvm \
   -DLLVM_BUILD_EXAMPLES=ON \
   -DLLVM_TARGETS_TO_BUILD="Native;NVPTX;AMDGPU" \
-  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_BUILD_TYPE=Debug \
   -DLLVM_ENABLE_ASSERTIONS=ON \
   -DCMAKE_C_COMPILER=clang \
   -DCMAKE_CXX_COMPILER=clang++ \
