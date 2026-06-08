505397f8292bc3c750f45d276dc194564fd62552
Kozma Márton <kozmamarton76@gmail.com>
2026-06-08 15:23:20 +0200

    Ext_adjust_u_v - measurement count set to 100

--- examples/pom2k/pom2k_adjust_u_v/benchmark_cpu_single_threaded.cpp
+++ examples/pom2k/pom2k_adjust_u_v/benchmark_cpu_single_threaded.cpp
@@ -96,7 +96,7 @@ static void print_first_10(const char* label, const float* values, size_t size)
 static const int rows = 4096*1;
 static const int cols = 4096*1;
 static const int levels = 20;
-static const int64_t numOfSamples = 10;
+static const int64_t numOfSamples = 100;
 
 int main(void)
 {
